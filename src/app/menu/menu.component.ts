1611f4ac586ae6f33cb8174e8973f6b31039a026
Louis Wilbrink <louis.wilbrink@gmail.com>
2019-07-15 11:03:07 -0700

    Menu progress

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -8,22 +8,42 @@ import anime from 'animejs/lib/anime.es.js';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+  public target: string;
+  public isOpen: boolean;
 
   constructor() { }
 
   ngOnInit() {
-    anime.set('.st0', {
-      translateX: '-50px',
+    this.target = '.top';
+    this.isOpen = false;
+
+    anime.set(this.target, {
+      translateX: '-100px',
+      translateY: '50px',
       scale: 1,
       rotate: '-45deg'
     });
-    
-    anime({
-      targets: '.st0',
-      scale: 25.5,
-      translateX: '-950px',
-      easing: 'easeOutQuad',
-      duration: 800
-    }); 
+  }
+
+  toggleMenu(isOpen) {
+    if (isOpen) {
+      anime({
+        targets: this.target,
+        scale: 12,
+        translateX: '-950px',
+        easing: 'easeOutQuad',
+        duration: 300
+      }); 
+    } else {
+      anime({
+        targets: this.target,
+        scale: 1,
+        translateX: '-100px',
+        easing: 'easeOutQuad',
+        duration: 300
+      }); 
+    }
+
+    this.isOpen = isOpen;
   }
 }
