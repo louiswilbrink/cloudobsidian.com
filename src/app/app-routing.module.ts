3cf26a14ab08f1698711ad7a4eb50324f0cb2037
Louis Wilbrink <louis.wilbrink@gmail.com>
2019-08-12 12:54:29 -0700

    Remove unused components; update top-level module; re-add animejs library.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,24 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // Components
-import { HeroComponent } from './hero/hero.component';
-import { PricingComponent } from './pricing/pricing.component';
-import { InfographicComponent } from './infographic/infographic.component';
 import { MenuComponent } from './menu/menu.component';
-import { LineDrawingComponent } from './line-drawing/line-drawing.component';
 
 const routes: Routes = [{
   path: 'home', 
-  component: InfographicComponent,
-}, {
-  path: 'pricing',
-  component: PricingComponent
+  component: MenuComponent,
 }, {
   path: 'menu',
   component: MenuComponent
-}, {
-  path: 'line-drawing',
-  component: LineDrawingComponent
 }, {
   path: '',
   redirectTo: '/home', 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroComponent } from './hero/hero.component';
-import { PricingComponent } from './pricing/pricing.component';
-import { InfographicComponent } from './infographic/infographic.component';
-import { LogoComponent } from './hero/logo/logo.component';
-import { LetterheadComponent } from './hero/letterhead/letterhead.component';
-import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
-import { LineDrawingComponent } from './line-drawing/line-drawing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent,
-    PricingComponent,
-    InfographicComponent,
-    LogoComponent,
-    LetterheadComponent,
-    HeaderComponent,
-    MenuComponent,
-    LineDrawingComponent
+    MenuComponent
   ],
   imports: [
     BrowserModule,
