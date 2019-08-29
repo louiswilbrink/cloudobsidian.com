78d9af4a92b1d9ce7f0b159353136f4d6becd658
Louis Wilbrink <louis.wilbrink@gmail.com>
2019-08-29 09:26:09 -0700

    Remove slide component declaration from module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { MenuComponent } from './menu/menu.component'
 import { HomeComponent } from './home/home.component';
 import { LinksComponent } from './links/links.component';
 import { ScrollMonitorDirective } from './scroll-monitor/scroll-monitor.directive';
-import { SlideComponent } from './slide/slide.component';
 import { LogoComponent } from './slides/logo/logo.component';
 import { AboutUsOneComponent } from './slides/about-us-one/about-us-one.component';
 import { AboutUsTwoComponent } from './slides/about-us-two/about-us-two.component';
@@ -29,7 +28,6 @@ import { ScrollIntoViewDirective } from './scroll-into-view/scroll-into-view.dir
     HomeComponent,
     LinksComponent,
     ScrollMonitorDirective,
-    SlideComponent,
     LogoComponent,
     AboutUsOneComponent,
     AboutUsTwoComponent,
