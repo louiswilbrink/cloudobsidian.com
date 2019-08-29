0d12e56a921bb9fcaf8fe3fcaad8c90482d5822b
Louis Wilbrink <louis.wilbrink@gmail.com>
2019-08-29 12:29:29 -0700

    Fix scroll offset issue.

--- src/app/logo/logo.component.ts
+++ src/app/logo/logo.component.ts
@@ -30,5 +30,4 @@ export class LogoComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
--- src/app/scroll-into-view/scroll-into-view.directive.ts
+++ src/app/scroll-into-view/scroll-into-view.directive.ts
@@ -1,13 +1,18 @@
-import { Directive, Input, ElementRef } from '@angular/core';
+import { Directive, Input, OnInit, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Directive({
   selector: '[scrollIntoView]'
 })
-export class ScrollIntoViewDirective {
+export class ScrollIntoViewDirective implements OnInit {
   @Input('scrollIntoView') anchor: string;
 
-  constructor(private route: ActivatedRoute, private elementRef: ElementRef) {
+  constructor(
+    private route: ActivatedRoute, 
+    private elementRef: ElementRef
+  ) { }
+
+  ngOnInit() {
     this.route.fragment.subscribe(fragment => {
       if (fragment === this.anchor) {
         this.elementRef.nativeElement.scrollIntoView();
