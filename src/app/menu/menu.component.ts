2ac52e28f0d6b21b109c2ff1d6d1097bbf23397f
Louis Wilbrink <louiswilbrink@Elisas-MacBook.local>
2019-09-13 00:23:30 -0700

    Redo mobile menu structure and animation.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,34 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
-import anime from 'animejs/lib/anime.es.js';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'co-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+	animations: [
+    trigger('expandCollapse', [
+      state(
+        'expand', 
+        style({ 
+          transform: 'translateY(0vh)'
+        })
+      ),
+      state(
+        'collapse', 
+        style({ 
+          transform: 'translateY(-90vh)'
+        })
+      ),
+      transition(
+        'void => expand, expand => collapse, collapse => expand', 
+        animate('200ms ease-in-out')
+      ),
+    ])
+  ]
 })
 export class MenuComponent implements OnInit {
-  public target: string;
   public isOpen: boolean;
 
   constructor() { }
 
   ngOnInit() {
-    this.target = '.top';
     this.isOpen = false;
   }
 
   toggleMenu(isOpen) {
-    console.log('toggleMenu');
-    anime({
-      targets: this.target,
-      easing: 'easeOutQuad',
-      duration: 300,
-      points: [{ 
-        value: '0,0 150,0 0,150',
-      }, { 
-        value: '0,0 375,0 0,375' 
-      }],
-    }); 
+    this.isOpen = !this.isOpen;
   }
 }
