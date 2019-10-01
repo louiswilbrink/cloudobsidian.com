a3b5e6c76b0935ac7e6a98b3b0084aa9314d0b39
Louis Wilbrink <louis.wilbrink@gmail.com>
2019-10-01 11:34:26 -0700

    Add mobile-logo component to process and services pages.

--- src/app/services-and-pricing/services-and-pricing.component.ts
+++ src/app/services-and-pricing/services-and-pricing.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { WindowRefService } from '../window-ref.service';
+
 @Component({
   selector: 'app-services-and-pricing',
   templateUrl: './services-and-pricing.component.html',
   styleUrls: ['./services-and-pricing.component.scss']
 })
 export class ServicesAndPricingComponent implements OnInit {
+  windowRef: any;
+  isMobile: boolean;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(windowRefService: WindowRefService) {
+    this.windowRef = windowRefService.nativeWindow;
   }
 
+  setMediaBehavior() {
+    let matchMedia = this.windowRef.matchMedia('(max-width: 414px)');
+
+    let setIsMobile = function (event) {
+      this.isMobile = event.matches;
+    }
+
+    let setIsMobileBinded = setIsMobile.bind(this);
+
+    this.isMobile = matchMedia.matches;
+
+    matchMedia.addListener(setIsMobileBinded);
+  }
+
+  ngOnInit() {
+    this.setMediaBehavior();
+  }
 }
