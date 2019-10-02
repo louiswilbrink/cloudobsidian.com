272ee1321f3a283f93683092bb8bd7938f8674ed
Louis Wilbrink <louis.wilbrink@gmail.com>
2019-10-01 22:59:11 -0700

    Add analytics tracking; add environment check;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router';
+
+import { environment } from '../environments/environment';
+
+declare let ga: Function;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cloud-obsidian';
+
+  constructor(public router: Router) {
+    if (environment.production) {
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          ga('set', 'page', event.urlAfterRedirects);
+          ga('send', 'pageview');
+        }
+      });
+    }
+  }
 }
